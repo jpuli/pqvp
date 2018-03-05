774e66aa971db5088922baa5a4ac9dc7acd48968
John Gedeon <john.gedeon@qualapps.com>
2018-03-05 12:56:38 -0800

    Initial commit

---
+++ Dockerfile
@@ -0,0 +1,3 @@
+FROM maven:3.5.2-jdk-9
+
+COPY . /pqvp
---
+++ src/main/java/com/qualapps/pqvp/PqvpApplication.java
@@ -0,0 +1,12 @@
+package com.qualapps.pqvp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PqvpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PqvpApplication.class, args);
+	}
+}
