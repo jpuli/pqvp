ae3bf0ed0d709abed0c1a49b92cbc614d3db4c0e
John Gedeon <john.gedeon@qualapps.com>
2018-03-12 09:47:08 -0700

    Update controller to return index.html
    https://qualappsinc.atlassian.net/browse/PQVP-48

--- src/main/java/com/qualapps/pqvp/IndexController.java
+++ src/main/java/com/qualapps/pqvp/IndexController.java
@@ -1,14 +1,15 @@
 package com.qualapps.pqvp;
 
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
-@RestController
+@Controller
 public class IndexController {
-
-    @RequestMapping("/")
-    public String index() {
-        return "Greetings from Spring Boot!";
-    }
-
+  @RequestMapping("/")
+  public String index(Model model, @RequestParam(value="name", required=false, defaultValue="John Appleseed") String name) {
+    model.addAttribute("name", name);
+    return "index";
+  }
 }
