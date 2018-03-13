1f90c1c9b4e9e8678b9b8dce9fe62da918f1e4a5
John Gedeon <john.gedeon@qualapps.com>
2018-03-13 10:29:23 -0700

    Move files from subproject to toplevel
    https://qualappsinc.atlassian.net/browse/PQVP-48

--- src/test/java/com/qualapps/pqvp/PqvpApplicationTests.java
+++ src/test/java/com/qualapps/pqvp/PqvpApplicationTests.java
@@ -1,4 +1,4 @@
-package com.qualapps.ka.article;
+package com.qualapps.pqvp;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class ArticleApplicationTests {
+public class PqvpApplicationTests {
 
 	@Test
 	public void contextLoads() {
