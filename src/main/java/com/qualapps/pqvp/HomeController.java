8169d657008af0f9d97400d1531289e3f3013052
John Gedeon <john.gedeon@qualapps.com>
2018-03-13 13:38:02 -0700

    Update controllers
    https://qualappsinc.atlassian.net/browse/PQVP-48

--- src/main/java/com/qualapps/pqvp/HomeController.java
+++ src/main/java/com/qualapps/pqvp/HomeController.java
@@ -1,21 +1,18 @@
 package com.qualapps.pqvp;
 
-import javax.servlet.http.HttpSession;
+import com.qualapps.ka.data.UserProfileData;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpSession;
 
 @Controller
 public class HomeController {
-    @GetMapping("/home")
-    public String home(HttpSession session, ModelMap modelMap) {
-        String userName = (String)session.getAttribute("userName");
-        modelMap.put("userName", userName);
-        return "home";
-    }
+  @GetMapping("/home")
+  public String home(HttpSession session, ModelMap modelMap) {
+    UserProfileData user = (UserProfileData) session.getAttribute("user");
+    modelMap.put("user", user);
+    return "home";
+  }
 }
--- src/main/java/com/qualapps/pqvp/LoginController.java
+++ src/main/java/com/qualapps/pqvp/LoginController.java
@@ -1,17 +1,26 @@
 package com.qualapps.pqvp;
 
-import javax.servlet.http.HttpSession;
+import com.qualapps.ka.common.PqvpException;
+import com.qualapps.ka.common.Utils;
+import com.qualapps.ka.data.UserProfileData;
+import com.qualapps.ka.service.UserProfileService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpSession;
 
 @Controller
 public class LoginController {
+  private final UserProfileService service;
+  private Utils utils = new Utils();
+
+  @Autowired
+  public LoginController(UserProfileService service) { this.service = service; }
+
   @GetMapping("/")
   public String index(Model model, @RequestParam(value="name", required=false, defaultValue="John Appleseed") String name) {
     model.addAttribute("name", name);
@@ -21,9 +30,14 @@ public class LoginController {
   }
 
   @PostMapping("/login")
-  public String login(Model model, HttpSession session, @RequestParam("userName") String userName) {
-    model.addAttribute("userName", userName);
-    session.setAttribute("userName", userName);
-    return "redirect:home";
+  public String login(HttpSession session, @RequestParam("userName") String userName) {
+    try {
+      UserProfileData user = service.getUserByName(userName);
+      session.setAttribute("user", user);
+      return "redirect:home";
+    } catch (PqvpException e) {
+      e.printStackTrace();
+      return "redirect:index";
+    }
   }
 }
